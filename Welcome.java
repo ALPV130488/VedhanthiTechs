8e026677889a0086db194b91ae2a27210be8bc01
LAKSHMI <lakshmi.l.msv@gmail.com>
2022-11-25 12:08:03 +0530

    second commit

---
+++ Welcome.java
@@ -0,0 +1,13 @@
+
+public class Welcome {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("lakshmi" .compareTo("laxmi"));//109-109
+		
+		//returns  -ve if obj1 has to come before obj2
+		//returns  +ve if obj1 has to come after obj2
+		//returns  0 if obj1 and obj2 are same.
+	}
+
+}
